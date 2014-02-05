ad80d158eafc42a32e6d9e2bf8d196e2af445fa1
Andrés Gomez <andresgomez92@gmail.com>
2014-02-05 20:43:35 +0100

    Fixed buffer size

--- processing/src/main/java/io/druid/query/aggregation/cardinality/DimensionCardinalityBufferAggregator.java
+++ processing/src/main/java/io/druid/query/aggregation/cardinality/DimensionCardinalityBufferAggregator.java
@@ -13,82 +13,79 @@ import java.util.List;
 /**
  *
  */
-public class DimensionCardinalityBufferAggregator implements BufferAggregator
-{
-  private static final byte[] emptyBytes;
+public class DimensionCardinalityBufferAggregator implements BufferAggregator {
 
-  static {
-    try {
-      emptyBytes = DimensionCardinalityAggregator.makeHllPlus().getBytes();
-    } catch (IOException e) {
-      throw Throwables.propagate(e);
-    }
-  }
+    private static final byte[] emptyBytes;
 
-  private final ObjectColumnSelector selector;
-
-  public DimensionCardinalityBufferAggregator(
-      ObjectColumnSelector selector
-  )
-  {
-    this.selector = selector;
-  }
-
-  @Override
-  public void init(ByteBuffer buf, int position)
-  {
-    ByteBuffer duplicate = buf.duplicate();
-    duplicate.position(position);
-    duplicate.putInt(emptyBytes.length);
-    duplicate.put(emptyBytes);
-  }
-
-  @Override
-  public void aggregate(ByteBuffer buf, int position)
-  {
-    HyperLogLogPlus hll = (HyperLogLogPlus) get(buf, position);
-
-    Object obj = selector.get();
-    if (obj == null) {
-      hll.offer(obj);
-    }
-    else if (obj instanceof List) {
-      for (Object o : (List) obj) {
-        hll.offer(o);
-      }
-    }
-    else if (obj instanceof HyperLogLogPlus) {
-      try {
-        hll.addAll((HyperLogLogPlus) obj);
-      } catch (CardinalityMergeException e) {
-        throw Throwables.propagate(e);
-      }
-    } else if (obj instanceof String) {
-      hll.offer(obj);
-    } else {
-      throw new UnsupportedOperationException(String.format("Unexpected object type[%s].", obj.getClass()));
+    static {
+        try {
+            emptyBytes = DimensionCardinalityAggregator.makeHllPlus().getBytes();
+        } catch (IOException e) {
+            throw Throwables.propagate(e);
+        }
     }
 
-  }
+    private final ObjectColumnSelector selector;
 
-  @Override
-  public Object get(ByteBuffer buf, int position)
-  {
-    ByteBuffer duplicate = buf.duplicate();
-    duplicate.position(position+4);
+    public DimensionCardinalityBufferAggregator(
+            ObjectColumnSelector selector
+    ) {
+        this.selector = selector;
+    }
 
-    return HyperLogLogPlus.Builder.build(duplicate);
-  }
+    @Override
+    public void init(ByteBuffer buf, int position) {
+        ByteBuffer duplicate = buf.duplicate();
+        duplicate.position(position);
+        duplicate.putInt(emptyBytes.length);
+        duplicate.put(emptyBytes);
+    }
 
-  @Override
-  public float getFloat(ByteBuffer buf, int position)
-  {
-    return ((HyperLogLogPlus) get(buf, position)).cardinality();
-  }
+    @Override
+    public void aggregate(ByteBuffer buf, int position) {
+        HyperLogLogPlus hll = (HyperLogLogPlus) get(buf, position);
 
-  @Override
-  public void close()
-  {
-  }
+        Object obj = selector.get();
+        if (obj == null) {
+            hll.offer(obj);
+        } else if (obj instanceof List) {
+            for (Object o : (List) obj) {
+                hll.offer(o);
+            }
+        } else if (obj instanceof HyperLogLogPlus) {
+            try {
+                hll.addAll((HyperLogLogPlus) obj);
+            } catch (CardinalityMergeException e) {
+                throw Throwables.propagate(e);
+            }
+        } else if (obj instanceof String) {
+            hll.offer(obj);
+        } else {
+            throw new UnsupportedOperationException(String.format("Unexpected object type[%s].", obj.getClass()));
+        }
+
+    }
+
+    @Override
+    public Object get(ByteBuffer buf, int position) {
+        ByteBuffer duplicate = buf.duplicate();
+        duplicate.position(position);
+
+        int size = duplicate.getInt();
+        duplicate.limit(size + position + 4);
+
+        ByteBuffer sliceBuff = duplicate.slice();
+
+        return HyperLogLogPlus.Builder.build(sliceBuff);
+    }
+
+    @Override
+    public float getFloat(ByteBuffer buf, int position) {
+        return ((HyperLogLogPlus) get(buf, position)).cardinality();
+    }
+
+    @Override
+    public void close() {
+    }
 
 }
